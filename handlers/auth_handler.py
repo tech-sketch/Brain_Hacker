76c05348e38630ff726be67e75da9f0c0a64094e
Hironsan <nakayama.hiroki@tis.co.jp>
2015-06-15 10:00:35 +0900

    buf fix: authentication

--- handlers/auth_handler.py
+++ handlers/auth_handler.py
@@ -1,3 +1,4 @@
+import sqlalchemy.orm.exc
 import bcrypt
 import tornado.web
 import tornado.escape
@@ -12,24 +13,19 @@ def is_authenticated(password, hashed_password):
 class LoginHandler(BaseHandler):
     def get(self):
         try:
-            errormessage = self.get_argument("error")
+            error_message = self.get_argument('error')
         except:
-            errormessage = ""
-        self.render("login.html", error_message = errormessage)
+            error_message = ''
+        self.render('login.html', error_message=error_message)
 
     def check_permission(self, password, email):
-        user = self.session.query(User).filter_by(email=email).first()
-        print(user.hashed_password)
-        print(type(user.hashed_password))
-        print(password)
-        print(is_authenticated(password, user.hashed_password))
-        return True
-        user = self.session.query(User).filter_by(email=email.encode('utf-8')).first()
-        print(user.name)
-
-        #if is_authenticated(password, user.hashed_password):
-            #return True
-        #return False
+        try:
+            user = self.session.query(User).filter_by(email=email).one()
+        except (sqlalchemy.orm.exc.NoResultFound, sqlalchemy.orm.exc.MultipleResultsFound):
+            return False
+        if is_authenticated(password, user.hashed_password):
+            return True
+        return False
 
     def post(self):
         email = self.get_argument('email', '')
@@ -40,9 +36,6 @@ class LoginHandler(BaseHandler):
             self.redirect(self.reverse_url(name='index'))
         else:
             self.render('login.html', error_message='Login Incorrect')
-            #self.redirect(self.reverse_url(name='login'))
-            #error_msg = u"?error=" + tornado.escape.url_escape("Login incorrect")
-            #self.redirect(u"/login/" + error_msg)
 
     def set_current_user(self, user):
         if user:
@@ -53,35 +46,35 @@ class LoginHandler(BaseHandler):
 
 class LogoutHandler(BaseHandler):
     def get(self):
-        self.clear_cookie("user")
+        self.clear_cookie('user')
         self.redirect(self.reverse_url(name='index'))
 
 
 class SignupHandler(BaseHandler):
     def get(self):
         try:
-            error_message = self.get_argument("error")
+            error_message = self.get_argument('error')
         except:
-            error_message = ""
+            error_message = ''
         print(id(self.session))
-        self.render("signup.html", error_message=error_message)
+        self.render('signup.html', error_message=error_message)
 
-    def __register(self, password, username, email):
-        username = username
-        password = password
-        email = email
-        # DBにメールアドレスが存在する場合False
-
-        hashed_password = bcrypt.hashpw(password=password.encode('utf-8'), salt=bcrypt.gensalt())
-        # validation and store db
-        return True
+    def can_regist(self, email):
+        try:
+            user = self.session.query(User).filter_by(email=email).one()
+        except (sqlalchemy.orm.exc.NoResultFound):
+            return True
+        return False
 
     def post(self):
         username = self.get_argument('username', '')
         password = self.get_argument('password', '')
         email    = self.get_argument('email', '')
-        hashed_password = bcrypt.hashpw(password=password.encode('utf-8'), salt=bcrypt.gensalt())
-        self.session.add(User(name=username, hashed_password=hashed_password, email=email))
-        self.session.commit()
-        #new = self.__register(password, username, email)
-        self.redirect(self.reverse_url('login'))
+        if self.can_regist(email):
+            hashed_password = bcrypt.hashpw(password=password.encode('utf-8'), salt=bcrypt.gensalt())
+            self.session.add(User(name=username, hashed_password=hashed_password, email=email))
+            self.session.commit()
+            self.redirect(self.reverse_url('login'))
+        else:
+            error_message = 'E-Mail Address already exists!'
+            self.render('signup.html', error_message=error_message)
