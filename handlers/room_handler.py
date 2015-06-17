f38f2cee12a3cd87655747dd3d4e41f1a16517e8
Hironsan <nakayama.hiroki@tis.co.jp>
2015-06-17 14:43:11 +0900

    add room edit and delete function

--- handlers/room_handler.py
+++ handlers/room_handler.py
@@ -29,6 +29,32 @@ class RoomHandler(BaseHandler):
     def get(self, group_id, room_id):
         self.render('room/room.html')
 
+class RoomEditHandler(BaseHandler):
+
+    @tornado.web.authenticated
+    def get(self, group_id, room_id):
+        group = self.session.query(Group).filter_by(id=group_id).first()
+        room = self.session.query(Room).filter_by(id=room_id).first()
+        self.render('room/room_edit.html', group=group, room=room)
+
+    @tornado.web.authenticated
+    def post(self, group_id, room_id):
+        name = self.get_argument('name', '')
+        room = self.session.query(Room).filter_by(id=room_id).first()
+        room.name = name
+        self.session.add(room)
+        self.session.commit()
+        self.redirect(self.reverse_url('rooms', group_id))
+
+
+class RoomDeleteHandler(BaseHandler):
+
+    @tornado.web.authenticated
+    def post(self, group_id, room_id):
+        room = self.session.query(Room).filter_by(id=room_id).first()
+        self.session.delete(room)
+        self.session.commit()
+        self.redirect(self.reverse_url('rooms', group_id))
 
 class RoomSocketHandler(tornado.websocket.WebSocketHandler):
     pass
--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from handlers.auth_handler import LoginHandler, LogoutHandler, SignupHandler
 from handlers.index_handler import IndexHandler
 from handlers.user_handler import UsersHandler, UserHandler
 from handlers.group_handler import GroupsHandler, GroupHandler, GroupUserHandler, GroupEditHandler, SearchNewMembersHandler
-from handlers.room_handler import RoomsHandler, RoomHandler, RoomSocketHandler
+from handlers.room_handler import RoomsHandler, RoomHandler, RoomEditHandler, RoomDeleteHandler, RoomSocketHandler
 
 from tornado.web import url
 
@@ -24,5 +24,7 @@ url_patterns = (
 
     url(r'/groups/([0-9]+)/rooms/', RoomsHandler, name='rooms'),
     url(r'/groups/([0-9]+)/rooms/([0-9]+)/', RoomHandler, name='room'),
+    url(r'/groups/([0-9]+)/rooms/([0-9]+)/edit', RoomEditHandler, name='room_edit'),
+    url(r'/groups/([0-9]+)/rooms/([0-9]+)/delete', RoomDeleteHandler, name='room_delete'),
     url(r'/groups/([0-9]+)/rooms/([0-9]+)/socket', RoomSocketHandler, name='room_socket'),
 )
