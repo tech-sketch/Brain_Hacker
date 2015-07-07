500d61e5eee097e57a3db013c9b229b3cc928ad1
Yuki-Inamoto <inamoto.yuki@tis.co.jp>
2015-07-07 10:11:55 +0900

    make test code

--- tests/handlers/test_auth_handler.py
+++ tests/handlers/test_auth_handler.py
@@ -1,6 +1,7 @@
 from unittest import mock
 from urllib import parse
 from tests.handlers.test_base import TestBase
+from pprint import pprint
 from handlers.base_handler import BaseHandler
 from http import cookies
 from tornado.testing import AsyncHTTPClient
@@ -25,62 +26,44 @@ class TestAuthHandlerBeforeLogin(TestBase):
         response = self.fetch(test_url, method='GET')
         self.assertEqual(response.code, 200)
 
+    def test_post_request_index(self):
+        # Bad Request
+        test_url = '/'
+        post_args = {}
+        response = self.fetch(test_url, method='POST',body=parse.urlencode(post_args))
+        self.assertEqual(response.code, 405)
+
     def test_get_request_login(self):
         test_url = '/auth/login/'
         response = self.fetch(test_url, method='GET')
         self.assertEqual(response.code, 200)
-    """
-    def test_post_request_login(self):
-        test_url = '/auth/login/'
-        post_args = {'username': 'test',
-                     'email': 'test@test.co.jp',
-                     'password': 'test'
-                     }
-        resp = self.fetch(test_url, method='POST', body=parse.urlencode(post_args), follow_redirects=False)
-        print(resp)
-        self.assertEqual(200, resp.code)
-    """
+
     def test_post_request_login(self):
         print("login post")
         test_url = '/auth/login/'
 
-        response = self.fetch(test_url, method='GET')
-        print(response.headers)
-        cookies = response.headers['Set-Cookie']
-
-        post_args = {'username': 'test',
-                     'email': 'test@test.co.jp',
-                     'password': 'test',
-                     '_xsrf': cookies
+        post_args = {'email': 'test@test.co.jp',
+                     'password': 'test'
                      }
 
         response = self.fetch(test_url, method='POST',
                               body=parse.urlencode(post_args),
                               follow_redirects=False,
                               )
-
-        print(response)
+        # print(response)
         self.assertEqual(response.code, 302)
-
         self.assertTrue(
             response.headers['Location'] == '/',
             "response.headers['Location'] did not ends with /"
         )
+
+    def test_get_request_signup(self):
+        test_url = '/auth/signup/'
+        response = self.fetch(test_url, method='GET')
+        self.assertEqual(response.code, 200)
     """
     def test_get_and_post_request_signup(self):
         test_url = '/auth/signup/'
-        response = self.fetch(test_url, method='GET')
-        print("GET")
-        print(response.headers['Set-Cookie'])
-        self.assertEqual(response.code, 200)
-
-        with mock.patch(BaseHandler, 'get_secure_cookie') as m:
-            json = BaseHandler.get_current_user(self)
-
-        print(json)
-
-        xsrf_cookies = response.headers['Set-Cookie']
-
         post_args = {'username': 'karateman7',
                      'email': '1ar@earth.co.jp',
                      'password': '10001000'
@@ -88,14 +71,16 @@ class TestAuthHandlerBeforeLogin(TestBase):
         response = self.fetch(test_url, method='POST',
                               body=parse.urlencode(post_args),
                               follow_redirects=False,
-                              headers={'Cookie': xsrf_cookies})
+                              )
         print(response)
         self.assertEqual(response.code, 302)
         self.assertTrue(
             response.headers['Location'].startswith('/auth/login/'),
             "response.headers['Location'] did not ends with /auth/login/")
-
+    """
     def test_get_request_logout(self):
+        # Bad Request
+        print("logout get")
         test_url = '/auth/logout/'
         response = self.fetch(test_url,  method='GET', follow_redirects=False)
         # print(response)
@@ -104,28 +89,49 @@ class TestAuthHandlerBeforeLogin(TestBase):
             response.headers['Location'].startswith('/auth/login/'),
             "response.headers['Location'] did not ends with /auth/login/"
         )
-"""
+
+    """
+     test_post_wrong_email_login(self):
+        input in forms
+        post_args = {
+                'email': 'wrong_test@test.co.jp',
+                'password': 'test'
+            }
+        We can observed by error message
+    """
+
+    """
+     test_post_wrong_password_login(self):
+        input in forms
+        post_args = {
+                'email': 'test@test.co.jp',
+                'password': 'wrong'
+            }
+        We can observed by error message
+    """
 
 class TestAuthHandlerAfterLogin(TestBase):
 
     def test_get_request_index(self):
+        print("login")
         response = self.user_login()
         user_cookie = response.headers['Set-Cookie']
 
         test_url = '/'
         response = self.fetch(test_url, method='GET', headers={'Cookie': user_cookie})
-        # print(response)
+        print(response)
         self.assertEqual(response.code, 200)
-
+    """
     def test_get_request_login(self):
         response = self.user_login()
         user_cookie = response.headers['Set-Cookie']
 
         test_url = '/auth/login/'
         response = self.fetch(test_url, method='GET', headers={'Cookie': user_cookie})
-        self.assertEqual(response.code, 200)
-
-    def test_post_request_logout(self):
+        self.assertEqual(response.code, 403)
+        # We can observed by status 200
+    """
+    def test_get_request_logout(self):
 
         response = self.user_login()
         user_cookie = response.headers['Set-Cookie']
--- tests/handlers/test_base.py
+++ tests/handlers/test_base.py
@@ -3,65 +3,24 @@ import tornado.web
 import tornado.escape
 
 from tornado.options import options
-from http import cookies
 
-import os
 from urls import url_patterns
 from urllib import parse
 
-from settings import settings
-
-
 class TestBase(tornado.testing.AsyncHTTPTestCase):
-    """
-    def __init__(self, *rest):
-        self.cookies = cookies.SimpleCookie()
-        tornado.testing.AsyncHTTPTestCase.__init__(self, *rest)
-    def _update_cookies(self, headers):
-        '''
-        from
-        https://github.com/peterbe/tornado-utils/blob/master/tornado_utils/http_test_client.py
-        '''
-        try:
-            sc = headers['Set-Cookie']
-            cookies = tornado.escape.native_str(sc)
-            self.cookies.update(cookies.SimpleCookie(cookies))
-            while True:
-                self.cookies.update(cookies.SimpleCookie(cookies))
-                if ',' not in cookies:
-                    break
-                cookies = cookies[cookies.find(',') + 1:]
-        except KeyError:
-            return
-
-    def fetch(self, url, *r, **kw):
-        if 'follow_redirects' not in kw:
-            kw['follow_redirects'] = False
-        header = {}
-        hs = self.cookies.output()
-        if hs != '':
-            hs = hs.split(':')[1]
-            header['Cookie'] = hs
-        resp = tornado.testing.AsyncHTTPTestCase.fetch(self, url, headers=header, *r, **kw)
-        self._update_cookies(resp.headers)
-        return resp
-    """
-
 
     def setUp(self):
         super(TestBase, self).setUp()
 
     def get_app(self):
-        #settings = dict()
-        self.app = tornado.web.Application(handlers=url_patterns, **settings)
+        self.app = tornado.web.Application(handlers=url_patterns, xsrf_cookies=False)
 
     def get_http_port(self):
         return options.port
 
     def user_login(self):
         login_url = '/auth/login/'
-        post_args = {'username': 'test',
-                     'email': 'test@test.co.jp',
+        post_args = {'email': 'test@test.co.jp',
                      'password': 'test'
                      }
         response = self.fetch(login_url, method='POST',
@@ -69,7 +28,7 @@ class TestBase(tornado.testing.AsyncHTTPTestCase):
                               follow_redirects=False)
         return response
 
-    def user_logout(self, user_cookie):
+    def user_logout(self):
         test_url = '/auth/logout/'
         response = self.fetch(test_url,  method='GET', follow_redirects=False)
         return response
