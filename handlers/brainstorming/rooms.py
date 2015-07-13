e0522e414d6001c857bb41234010ca4b635e19ac
ko-he- <tie304355@S1504C10257-T1.tisnt.tis.co.jp>
2015-07-13 13:04:02 +0900

    add number of people that are participating in the brainstorming

--- handlers/brainstorming/rooms.py
+++ handlers/brainstorming/rooms.py
@@ -8,13 +8,16 @@ class Rooms(object):
     def __init__(self):
         """
         self.user_on_rooms = {
-            room_id1: set( client_name),
+            room_id1: set(client_name),
             room_id2: set(),
             ...
             room_idn: set()
         }
         """
         self.users_in_rooms = defaultdict(set)
+
+    def count_user_already_in_room_of(self, room_id):
+        return len(self.rooms[room_id])
 
     def checks_user_already_in_room_of(self, room_id, client_name):
         return client_name in self.users_in_rooms[room_id]
@@ -35,7 +38,10 @@ class Rooms(object):
     def remove_client(self, client):
         room_id = self.clients[client]
         del self.clients[client]
+        print("-----------------------")
+        print(self.rooms[room_id])
         self.rooms[room_id] -= set([client])
+        print(self.rooms[room_id])
 
     def clear_users_in_room(self, room_id):
         del self.users_in_rooms[room_id]
--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ from tornado.options import define, options
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-define('port', default=8817, help="run on the given port", type=int)
+define('port', default=8888, help="run on the given port", type=int)
 define('debug', default=True, help='debug mode')
 tornado.options.parse_command_line()
 
